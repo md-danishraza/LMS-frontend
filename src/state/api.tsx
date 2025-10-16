a501ca0d33479317b8fdc751af8dc7e7237096a0
Md Danish Raza <md.danish0raza@gmail.com>
2025-10-16 21:24:16 +0530

    types and initial queries

--- src/state/api.tsx
+++ src/state/api.tsx
@@ -16,10 +16,24 @@ export const baseQuery = fetchBaseQuery({
 export const api = createApi({
   reducerPath: "api",
   baseQuery,
-  tagTypes:[],
+  tagTypes:["Courses"],
   endpoints: (builder) => ({
-    // queries
+    // query<returnType,inputType>
+    getCourses:builder.query<Course[],{category?:string}>(
+      {
+        query:({category}) => ({
+          url:"courses",  
+          params:{category}
+        }),
+        providesTags:["Courses"]
+      }
+    ),
+    getCourse:builder.query<Course,string>({
+      query:(id)=> `courses/${id}`,
+      // invalidate courses if theres change in specific course
+      providesTags:(result,error,id)=>[{type:"Courses",id}]
+    })
   }),
 });
 
-export const {} = api;
+export const {useGetCoursesQuery,useGetCourseQuery} = api;
